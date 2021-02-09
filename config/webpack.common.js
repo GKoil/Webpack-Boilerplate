7306a7b5c0a0d482593f1af5ea16f0ff130a0a58
Кирилл <samsuu@ya.ru>
2021-02-09 15:16:36 +0700

    Add v 0.7
    1. Add eslint
    2. Fix config files

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -2,6 +2,7 @@ const path = require('path');
 const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, '../src/index.js'),
@@ -9,17 +10,21 @@ module.exports = {
     rules: [
       {
         test: /\.scss$/,
-        use: [ 'style-loader', 'css-loader', 'sass-loader' ],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
   plugins: [
     new CleanWebpackPlugin(),
-    new LodashModuleReplacementPlugin,
+    new LodashModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
       title: 'webpack Boilerplate',
       template: path.resolve(__dirname, '../src/template.html'),
       filename: 'index.html',
+    }),
+    new ESLintPlugin({
+      files: ['.', 'src', 'config'],
+      formatter: 'table',
     }),
   ],
 };
